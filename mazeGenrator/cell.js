1a67e6b1ad319b70940482a6c47fd4c575b4dc32
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2016-12-06 19:42:21 -0600

    mazeGenerator: visit neighboring cells and color them

--- mazeGenrator/cell.js
+++ mazeGenrator/cell.js
@@ -1,32 +1,77 @@
 function Cell(i, j) {
+  // each cell has x, y coordinate and four walls
   this.i = i;
   this.j = j;
+  this.walls = [true, true, true, true]; // status of top, right, bottom, left walls
+  this.visited = false;
+
+  this.index = function(i, j) {
+    if (i < 0 || j < 0 || i > cols - 1 || j > rows -1) {
+      return -1;
+    }
+    return i + j * cols;
+  }
+
+  this.checkNeighbors = function() {
+    var neghbors = [];
+
+    var top = grid[this.index(i, j - 1)];
+    var right = grid[this.index(i+1, j)];
+    var bottom = grid[this.index(i, j + 1)];
+    var left = grid[this.index(i - 1, j)];
+
+    if (top && !top.visited) {
+      neghbors.push(top);
+    }
+    if (right && !right.visited) {
+      neghbors.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neghbors.push(bottom);
+    }
+    if (left && !left.visited) {
+      neghbors.push(left);
+    }
+
+    if  (neghbors.length > 0) {
+      var r = floor(random(0, neghbors.length));
+      return neghbors[r];
+    }
+    else {
+      return undefined;
+    }
+  }
 
   this.show = function() {
     var x = this.i * w;
     var y = this.j * w;
-    this.walls = [true, true, true, true]; // top, right, bottom, left
 
     stroke(255);
 
+    // check the status of the wall, if true draw the wall
+
     if (this.walls[0]) {
-      line(x,     y,     x + w, y);     // top wall
+      line(x,     y,     x + w, y);     // draw top wall
     }
 
     if (this.walls[1]) {
-      line(x + w, y,     x + w, y + w); // right wall
+      line(x + w, y,     x + w, y + w); // draw right wall
     }
 
     if (this.walls[2]) {
-     line(x + w, y + w, x,     y + w); // bottom wall
+     line(x + w, y + w, x,     y + w); // draw bottom wall
     }
 
     if (this.walls[3]) {
-      line(x,     y + w, x,     y);     // left wall
+      line(x,     y + w, x,     y);     // draw left wall
     }
 
-    //noFill();
-    //rect(x, y, w, w);
+    // if cell is visited, then draw a colored rectangle
+    if (this.visited) {
+      console.log("here");
+      fill(255, 0, 255, 100);
+      rect(x, y, w, w);
+    }
 
   }
 }
--- mazeGenrator/mazeGenerator.js
+++ mazeGenrator/mazeGenerator.js
@@ -3,11 +3,13 @@ var w = 40; // so we have a canvas of 400 by 400, and row, columns are 40 by 40
             // so there will be 10 rows and  10 cols
 
 var grid = [];
+var current;
 
 function setup() {
   createCanvas(400, 400);
   cols = floor(width/ w);
   rows = floor(width/ w);
+  frameRate(1);
 
   for (var j = 0; j < cols; j++) {
     for (var i = 0; i < rows; i++) {
@@ -15,11 +17,20 @@ function setup() {
       grid.push(cell);
     }
   }
+
+  current = grid[0];
 }
 
 function draw() {
   background(51);
   for (var i = 0; i < grid.length; i++) {
     grid[i].show();
+  }
+
+  current.visited = true;
+  var next = current.checkNeighbors();
+
+  if (next) {
+    current = next;
   }
 }
