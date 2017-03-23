b12c5caccffae78d8f1ed4696c7e049557b81e68
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2017-03-22 23:32:22 -0500

    Add death method in snake when it touches it self

--- snakeGame/snake.js
+++ snakeGame/snake.js
@@ -3,8 +3,18 @@ function Snake() {
   this.y = 0;
   this.xspeed = 1;
   this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
 
   this.update = function() {
+    if (this.total === this.tail.length) {
+      for (var i = 0; i < this.tail.length-1; i++) {
+        this.tail[i] = this.tail[i+1];
+      }
+    }
+
+    this.tail[total-1] = createVector(this.x, this.y);
+
     this.x = this.x + this.xspeed*scl;
     this.y = this.y + this.yspeed*scl;
 
@@ -14,6 +24,9 @@ function Snake() {
 
   this.show = function() {
     fill(255);
+    for (var i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
     rect(this.x, this.y, scl, scl);
   }
 
@@ -25,10 +38,23 @@ function Snake() {
   this.eat = function(pos) {
     var d = dist(this.x, this.y, pos.x, pos.y);
     if (d < 1) {
+      this.total++;
       return true;
     }
     else {
       return false;
+    }
+  }
+
+  this.death = function() {
+    for (var i = 0; i < this.tail.length; i++) {
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+
+      if (d < 1) {
+        this.total = 0;
+        this.tail = [];
+      }
     }
   }
 }
