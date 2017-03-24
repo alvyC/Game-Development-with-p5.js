c4aa40934f9b1cf323bee0c62bd807f703426b71
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2017-03-24 15:30:26 -0500

    Complete snake game

--- snakeGame/snakeGame.js
+++ snakeGame/snakeGame.js
@@ -1,45 +1,54 @@
-var snake;
+var s;
 var scl = 20;
+
 var food;
 
 function setup() {
-  createCanvas(600, 400);
-  snake = new Snake();
+  createCanvas(600, 600);
+  s = new Snake();
   frameRate(10);
-  food = createVector(random(width), random(height));
+  pickLocation();
+
+}
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
 }
 
 function draw() {
   background(51);
-  snake.update();
-  snake.show();
 
-  if (snake.eat(food)) {
-    food = pickLocation();
+  if (s.eat(food)) {
+    pickLocation();
   }
+  s.death();
+  s.update();
+  s.show();
+
 
   fill(255, 0, 100);
   rect(food.x, food.y, scl, scl);
 }
 
-function pickLocation() {
-  var cols = floor(width/ scl);
-  var rows = floor(height/ scl);
-  food = createVector(random(width), random(height));
-  food.mult(scl);
-}
+
+
+
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    snake.dir(0, -1); // 0 on the x-axis and 1 on the up (so -1)
-  }
-  else if (keyCode === DOWN_ARROW) {
-    snake.dir(0, 1);
-  }
-  else if (keyCode === RIGHT_ARROW) {
-    snake.dir(1, 0);
-  }
-  else if (keyCode === LEFT_ARROW) {
-    snake.dir(-1, 0);
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
   }
 }
