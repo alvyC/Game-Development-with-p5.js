c1372e5d0fa7a2a75b4b4a844e91dc85a757f2f0
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2017-04-02 23:33:54 -0500

    Add keys, secret and put auth info in config

---
+++ twitterBot/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'XquPHvYKjkyBDbgk4hX2NtU9P',
+  consumer_secret:      'hPzt0eqnxeLsFnvalzneLn7U5AOUKUsIBEufZfhOXjo6RkKWkE',
+  access_token:         '795142420153454592-Rxz26xsJ9JxHxSqopIlD0iqtdoxDugM',
+  access_token_secret:  '0athRro0ejFqawqvwlbDhmEVYdpHcEnTsGE2Dl0w5g9Se',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
