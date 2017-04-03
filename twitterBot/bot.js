9897a40123653647476e05b8dcd225f394096560
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2017-04-03 00:09:55 -0500

    Post to twiitter, add .gitingonore

--- twitterBot/bot.js
+++ twitterBot/bot.js
@@ -6,3 +6,29 @@ var config = require('./config');
 console.log(config);
 
 var T = new Twit(config);
+
+/* Get tweet */
+var params = {
+  q: 'rainbow',
+  count: 100
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].text);
+  }
+}
+
+/* Post tweet */
+var tweet = {
+  status: '#codingrainbow from node.js'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response) {
+  console.log(data);
+}
