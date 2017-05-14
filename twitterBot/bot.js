343f1687d727a93511e88b1c14713806c9eaf818
Muktadir R Chowdhury <mrchwdhr@memphis.edu>
2017-05-14 00:04:00 -0500

    twitter-bot: reply when someone follows me

--- twitterBot/bot.js
+++ twitterBot/bot.js
@@ -10,7 +10,7 @@ var T = new Twit(config);
 /* Get tweet */
 var params = {
   q: 'rainbow',
-  count: 100
+  count: 2
 }
 
 T.get('search/tweets', params, gotData);
@@ -22,13 +22,55 @@ function gotData(err, data, response) {
   }
 }
 
-/* Post tweet */
-var tweet = {
-  status: '#codingrainbow from node.js'
+// setting up a user stream
+var stream = T.stream('user');
+
+// Anytime someone follows me
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+  console.log("Follow event!");
+  var name = eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name;
+  replyFollower('@' + screenName + ' do you like rainbows?');
 }
 
-//T.post('statuses/update', tweet, tweeted);
+// setInterval(tweetIt, 1000*20); // If we want to tweet in every 20 seconds.
+// tweetIt();
 
-function tweeted(err, data, response) {
-  console.log(data);
+/* Post tweet */
+function tweetIt() {
+  var r = Math.floor(Math.random()*1000);
+
+  var tweet = {
+    status: 'here is a random number ' + r + ' #codingrainbow from node.js'
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("Something went wrong!!");
+    }
+    else {
+      console.log("It worked.");
+    }
+  }
+}
+
+function replyFollower(txt) {
+  var tweet = {
+    status: txt
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("Something went wrong!!");
+    }
+    else {
+      console.log("It worked.");
+    }
+  }
 }
